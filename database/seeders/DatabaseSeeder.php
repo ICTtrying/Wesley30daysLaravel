ff56de39ead9595b18a75e24dad39fb8e6f567fd
Wesley <340308@student.mboutrecht.nl>
2025-09-14 22:42:47 +0200

    video 15

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -16,8 +15,11 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'first_name' => 'Jhon',
+            'last_name' => 'Doe',
+            'email' => 'email@example.com'
         ]);
+
+        $this->call(JobSeeder::class);
     }
 }
